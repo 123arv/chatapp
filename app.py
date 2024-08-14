68168fcceebffddd5736646552d886fe02c742cf
123arv <134442786+123arv@users.noreply.github.com>
2024-08-15 02:34:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,53 +1,19 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+from flask import Flask, render_template, request
 from flask_socketio import SocketIO, join_room, leave_room, send
-from flask_sqlalchemy import SQLAlchemy
+import gevent
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='gevent')
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    password = db.Column(db.String(150), nullable=False)
-
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username, password=password).first()
-        if user:
-            session['username'] = username
-            return redirect(url_for('chat', username=username, room='General'))
-        else:
-            flash('Invalid credentials. Please try again.')
-    return render_template('login.html')
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        if User.query.filter_by(username=username).first():
-            flash('Username already exists.')
-        else:
-            new_user = User(username=username, password=password)
-            db.session.add(new_user)
-            db.session.commit()
-            session['username'] = username
-            return redirect(url_for('chat', username=username, room='General'))
-    return render_template('register.html')
+    return render_template('index.html')
 
 @app.route('/chat')
 def chat():
-    if 'username' not in session:
-        return redirect(url_for('index'))
-    username = session['username']
-    room = request.args.get('room', 'General')
+    username = request.args.get('username')
+    room = request.args.get('room')
     return render_template('chat.html', username=username, room=room)
 
 @socketio.on('join')
@@ -71,13 +37,7 @@ def handle_message(data):
     username = data['username']
     send(f'{username}: {msg}', to=room)
 
-@app.route('/logout')
-def logout():
-    session.pop('username', None)
-    return redirect(url_for('index'))
-
 if __name__ == '__main__':
     import os
     port = int(os.environ.get('PORT', 5000))
-    socketio.run(app, host='0.0.0.0', port=port, debug=True)
-
+    socketio.run(app, host='0.0.0.0', port=port)
